0802a8faaad71304fb22a7851a27e8cabd20a9df
irSilviana <76592439+irSilviana@users.noreply.github.com>
2021-03-29 23:14:33 +0700

    Added function to calculate expenses & remaining budget

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import { TiDelete } from "react-icons/ti";
+import { AppContext } from "../Context/AppContext";
 
 export default function ExpenseItem(props) {
+  const { dispatch } = useContext(AppContext);
+
+  const handleDeleteExpense = () => {
+    dispatch({
+      type: "DELETE_EXPENSE",
+      payload: props.id,
+    });
+  };
+
   return (
     <div className="ExpenseItem ">
       <li className="list-group-item d-flex justify-content-between align-items-center">
         {props.name}
         <span className="badge badge-primary badge-pill mr-3">
           ${props.cost}
-          <TiDelete size="1.5em"></TiDelete>
+          <TiDelete size="1.5em" onClick={handleDeleteExpense}></TiDelete>
         </span>
       </li>
     </div>
--- src/Components/ExpenseTotal.js
+++ src/Components/ExpenseTotal.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../Context/AppContext";
 
 export default function ExpenseTotal() {
+  const { expenses } = useContext(AppContext);
+
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total += item.cost);
+  }, 0);
+
   return (
     <div className="ExpenseTotal alert alert-primary">
-      <span>Spent so far: $850</span>
+      <span>Spent so far: ${totalExpenses}</span>
     </div>
   );
 }
